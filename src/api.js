84992e52da7ab0d028d5748931b63ab7415ac9e9
unknown <reman1999@outlook.com>
2023-03-08 21:18:24 +0100

    Fixed getEvents typo error

--- src/api.js
+++ src/api.js
@@ -52,9 +52,8 @@ export const getEvents = async () => {
   if (token) {
     removeQuery();
     const url =
-      'https://m4w3mcnu50.execute-api.eu-central-1.amazonaws.com/dev/api/get-events' +
-      '/' +
-      token;
+      'https://m4w3mcnu50.execute-api.eu-central-1.amazonaws.com/dev/api/get-events';
+    token;
     const result = await axios.get(url);
     if (result.data) {
       var locations = extractLocations(result.data.events);
