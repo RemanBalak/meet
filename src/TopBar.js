519435e5269cef36e6b33a334ec486df59465888
unknown <reman1999@outlook.com>
2023-03-09 01:28:55 +0100

    Deleted Themechanger

--- src/TopBar.js
+++ src/TopBar.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
-import ThemeChanger from './ThemeChanger';
 
 class TopBar extends Component {
   render() {
     return (
       <div className="TopBar">
         <h1>Meet App</h1>
-        <ThemeChanger />
       </div>
     );
   }
