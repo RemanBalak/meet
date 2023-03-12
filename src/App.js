dbeab1e3bf3e651cce57392201c7b39f76b930af
unknown <reman1999@outlook.com>
2023-03-12 08:43:41 +0100

    FIxed app.js

--- src/App.js
+++ src/App.js
@@ -106,31 +106,7 @@ class App extends Component {
             updateNumberOfEvents={(num) => this.updateNumberOfEvents(num)}
           />
         </div>
-        <div className="data-vis-wrapper">
-          <EventGenre events={this.state.events} />
 
-          <ResponsiveContainer height={400}>
-            <ScatterChart
-              margin={{
-                top: 20,
-                right: 20,
-                bottom: 20,
-                left: 20,
-              }}
-            >
-              <CartesianGrid />
-              <XAxis type="category" dataKey="city" name="city" />
-              <YAxis
-                type="number"
-                dataKey="number"
-                name="number of events"
-                allowDecimals={false}
-              />
-              <Tooltip cursor={{ strokeDasharray: '3 3' }} />
-              <Scatter data={this.getData()} fill="#8884d8" />
-            </ScatterChart>
-          </ResponsiveContainer>
-        </div>
         <EventList events={this.state.events} />
         <WelcomeScreen
           showWelcomeScreen={this.state.showWelcomeScreen}
